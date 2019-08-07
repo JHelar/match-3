3af431b2aa75bd3b5636cafdc311df168c87dc8f
JHelar <john.helarsson@gmail.com>
2019-08-07 17:21:27 +0200

    Removed ignored folders

--- compiled/elements.js
+++
@@ -1,4 +0,0 @@
-export const gameElement = document.getElementById('game');
-export const tileTemplate = document.querySelector('#tile');
-export const nodeTemplate = document.querySelector('#node');
-//# sourceMappingURL=elements.js.map
